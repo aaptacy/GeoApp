e24aac52512f777aa5fa6723b8fb5bc2ed0395e4
Albert Aptacy <albertaptacy@gmail.com>
2018-03-13 22:09:09 +0100

    Modified inputs into 'required' and changed placeholders to zeros

--- index.php
+++ index.php
@@ -1,13 +1,11 @@
 <?php
-    include 'distance.php';
+    include 'src/distance.php';
 ?>
 <!DOCTYPE html>
 <html lang="en" style="height: 100%; background-size: 100%; background: linear-gradient(to bottom, deepskyblue 0%, snow 100%) no-repeat">
 <head>
     <meta charset="UTF-8">
     <title>GeoApp</title>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="distance.js"></script>
 </head>
 <body style="height: 100%">
 <h2>Witaj w GeoApp i oblicz odległość między dwoma dowolnymi współrzędnymi na Ziemi</h2>
@@ -16,36 +14,36 @@
     <h3>Wprowadź współrzędne pierwszego miejsca</h3>
     Szerokość geograficzna:
     <br>
-    <input style="width: 60px" type="number" name="degrees1lat" min="0" max="90" step="1" placeholder="50">&deg;
-    <input style="width: 60px" type="number" name="minutes1lat" min="0" max="59" step="1" placeholder="30">'
-    <input style="width: 60px" type="number" name="seconds1lat" min="0" max="59" step="0.1" placeholder="15.4">"
-    <input type="radio" name="north_south1" value="1"/>N
-    <input type="radio" name="north_south1" value="-1"/>S
+    <input style="width: 60px" type="number" name="degrees1lat" min="0" max="90" step="1" placeholder="0">&deg;
+    <input style="width: 60px" type="number" name="minutes1lat" min="0" max="59" step="1" placeholder="0">'
+    <input style="width: 60px" type="number" name="seconds1lat" min="0" max="59" step="0.1" placeholder="0.0">"
+    <input type="radio" name="north_south1" value="1" required/>N
+    <input type="radio" name="north_south1" value="-1" required/>S
     <br>
     Długość geograficzna:
     <br>
-    <input id="input" style="width: 60px" type="number" name="degrees1long" min="0" max="180" step="1" placeholder="120">&deg;
-    <input style="width: 60px" type="number" name="minutes1long" min="0" max="59" step="1" placeholder="30">'
-    <input style="width: 60px" type="number" name="seconds1long" min="0" max="59" step="0.1" placeholder="15.4">"
-    <input type="radio" name="east_west1" value="-1">E
-    <input type="radio" name="east_west1" value="1">W
+    <input id="input" style="width: 60px" type="number" name="degrees1long" min="0" max="180" step="1" placeholder="0">&deg;
+    <input style="width: 60px" type="number" name="minutes1long" min="0" max="59" step="1" placeholder="0">'
+    <input style="width: 60px" type="number" name="seconds1long" min="0" max="59" step="0.1" placeholder="0.0">"
+    <input type="radio" name="east_west1" value="-1" required>E
+    <input type="radio" name="east_west1" value="1" required>W
     <br>
     <h3>Wprowadź współrzędne drugiego miejsca</h3>
     Szerokość geograficzna:
     <br>
-    <input style="width: 60px" type="number" name="degrees2lat" min="0" max="90" step="1" placeholder="50">&deg;
-    <input style="width: 60px" type="number" name="minutes2lat" min="0" max="59" step="1" placeholder="30">'
-    <input style="width: 60px" type="number" name="seconds2lat" min="0" max="59" step="0.1" placeholder="15.4">"
-    <input type="radio" name="north_south2" value="1">N
-    <input type="radio" name="north_south2" value="-1">S
+    <input style="width: 60px" type="number" name="degrees2lat" min="0" max="90" step="1" placeholder="0">&deg;
+    <input style="width: 60px" type="number" name="minutes2lat" min="0" max="59" step="1" placeholder="0">'
+    <input style="width: 60px" type="number" name="seconds2lat" min="0" max="59" step="0.1" placeholder="0.0">"
+    <input type="radio" name="north_south2" value="1" required>N
+    <input type="radio" name="north_south2" value="-1" required>S
     <br>
     Długość geograficzna:
     <br>
-    <input style="width: 60px" type="number" name="degrees2long" min="0" max="180" step="1" placeholder="120">&deg;
-    <input style="width: 60px" type="number" name="minutes2long" min="0" max="59" step="1" placeholder="30">'
-    <input style="width: 60px" type="number" name="seconds2long" min="0" max="59" step="0.1" placeholder="15.4">"
-    <input type="radio" name="east_west2" value="-1">E
-    <input type="radio" name="east_west2" value="1">W
+    <input style="width: 60px" type="number" name="degrees2long" min="0" max="180" step="1" placeholder="0">&deg;
+    <input style="width: 60px" type="number" name="minutes2long" min="0" max="59" step="1" placeholder="0">'
+    <input style="width: 60px" type="number" name="seconds2long" min="0" max="59" step="0.1" placeholder="0.0">"
+    <input type="radio" name="east_west2" value="-1" required>E
+    <input type="radio" name="east_west2" value="1" required>W
     <br>
     <input type="submit" id="button" value="Oblicz odległość">
 </form>
---
+++ src/distance.php
@@ -0,0 +1,43 @@
+<?php
+
+function sanitizeData($postName, $variableType)
+    {
+        if ($variableType === 'int') {
+            return filter_input(INPUT_POST, $postName, FILTER_SANITIZE_NUMBER_INT);
+        }
+        if($variableType === 'float') {
+          return filter_input(INPUT_POST, $postName, FILTER_SANITIZE_NUMBER_FLOAT);
+        }
+    }
+
+    function getDistance()
+    {
+            $aPoint['longitude'] = sanitizeData('degrees1long', 'int')
+                +sanitizeData('minutes1long', 'int')/60
+                +sanitizeData('seconds1long', 'float')/3600;
+            $aPoint['latitude'] = sanitizeData('degrees1lat', 'int')
+                +sanitizeData('minutes1lat', 'int')/60
+                +sanitizeData('seconds1lat', 'float')/3600;
+
+            $bPoint['longitude'] =   sanitizeData('degrees2long', 'int')
+                +sanitizeData('minutes2long', 'int')/60
+                +sanitizeData('seconds2long', 'float')/3600;
+            $bPoint['latitude'] = sanitizeData('degrees2lat', 'int')
+                +sanitizeData('minutes2lat', 'int')/60
+                +sanitizeData('seconds2lat', 'float')/3600;
+
+            $earthRadius = 6371.137;// promień Ziemi w kilometrach
+
+            $a = $_POST['north_south1'];
+            $along = $_POST['east_west1'];
+            $b = $_POST['north_south2'];
+            $blong = $_POST['east_west2'];
+
+            $distance = (acos(sin(deg2rad($a*$aPoint['latitude']))
+                    *sin(deg2rad($b*$bPoint['latitude']))
+                    +cos(deg2rad($a*$aPoint['latitude']))
+                    *cos(deg2rad($b*$bPoint['latitude']))
+                    *cos(deg2rad($blong*$bPoint['longitude']-$along*$aPoint['longitude'])))*$earthRadius);
+
+            return number_format($distance,4,'.','');
+    }
